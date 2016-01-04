f2da2bd73348ad3ecc7ff81650539b44de590ac7
Zhi Zeng <zengzhi_2012@hotmail.com>
2016-01-04 13:39:36 +0800

    Create CommonLibs.h

---
+++ CommonLibs.h
@@ -0,0 +1,40 @@
+/*
+ * CommonLibs.h
+ *
+ *      Author: ZENG
+ */
+
+#ifndef BACKGROUNDSUBTRACTION_C_MOTION_TLI_1_0_COMMONLIBS_H_
+#define BACKGROUNDSUBTRACTION_C_MOTION_TLI_1_0_COMMONLIBS_H_
+
+#include <opencv2/opencv.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/core/types.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/core/cuda.hpp"
+#include "opencv2/superres/optical_flow.hpp"
+#include "opencv2/superres.hpp"
+#include "opencv2/video/tracking.hpp"
+#include "cv.h"
+#include "highgui.h"
+#include "imgproc.hpp"
+#include "cxcore.h"
+#include <math.h>
+#include <cmath>
+#include <iostream>
+#include <cctype>
+#include <ctype.h>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <list>
+#include <vector>
+#include <map>
+#include <stack>
+
+using namespace std;
+using namespace cv;
+
+#endif /* BACKGROUNDSUBTRACTION_C_MOTION_TLI_1_0_COMMONLIBS_H_ */
